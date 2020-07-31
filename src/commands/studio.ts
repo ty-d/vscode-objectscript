aecaed1a605aa073f0e26667b3a15cf0153c0ffa
Tyler Deemer <tdeemer@intersystems.com>
2020-07-31 12:34:04 -0400

    Open CSP pages

--- src/commands/studio.ts
+++ src/commands/studio.ts
@@ -250,42 +250,44 @@ class StudioActions {
         title: `Executing user action: ${action.label}`,
       },
       () => {
-        return this.api
-          .actionQuery(query, parameters)
-          .then(async (data) => {
-            if (action.save) {
-              await this.processSaveFlag(action.save);
-            }
-            if (!afterUserAction) {
-              outputConsole(data.console);
-            }
-            if (!data.result.content.length) {
-              // nothing to-do, just ignore it
-              return;
-            }
-            const actionToProcess = data.result.content.pop();
+        return new Promise((resolve, reject) => {
+          this.api
+            .actionQuery(query, parameters)
+            .then(async (data) => {
+              if (action.save) {
+                await this.processSaveFlag(action.save);
+              }
+              if (!afterUserAction) {
+                outputConsole(data.console);
+              }
+              if (!data.result.content.length) {
+                // nothing to-do, just ignore it
+                return;
+              }
+              const actionToProcess = data.result.content.pop();
 
-            // CSP pages should not have a progress bar
-            if (actionToProcess.action === 2) {
-              return;
-            }
-            return actionToProcess;
-          })
-          .then(
-            (actionToProcess) =>
-              actionToProcess &&
-              this.processUserAction(actionToProcess).then((answer) =>
-                answer && (answer.msg || answer.msg === "")
+              // CSP pages should not have a progress bar
+              if (actionToProcess.action === 2) {
+                resolve();
+              } else if (actionToProcess.action === 0) {
+                return Promise.resolve();
+              }
+
+              const answer = await this.processUserAction(actionToProcess);
+              if (answer) {
+                return answer.msg || answer.msg === ""
                   ? this.userAction(action, true, answer.answer, answer.msg, type)
-                  : this.userAction(action, true, answer, "", type)
-              )
-          )
-          .catch((err) => {
-            console.log(err);
-            outputChannel.appendLine(`Studio Action "${action.label}" not supported`);
-            outputChannel.show();
-            return Promise.reject();
-          });
+                  : this.userAction(action, true, answer, "", type);
+              }
+            })
+            .then(() => resolve())
+            .catch((err) => {
+              console.log(err);
+              outputChannel.appendLine(`Studio Action "${action.label}" not supported`);
+              outputChannel.show();
+              reject();
+            });
+        });
       }
     );
   }
